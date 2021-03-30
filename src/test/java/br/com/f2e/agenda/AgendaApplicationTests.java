05f1b22cafbaddddf4878984b4a040d92d673b2a
Franklin Barreto <franklin-barreto@hotmail.com>
2021-03-30 09:49:57 -0300

    feat: Commit inicial do projeto

---
+++ src/test/java/br/com/f2e/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.f2e.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
